026456a25d2cd79f34bfe43182e3def8302c4571
d-valles <davalles27@gmail.com>
2020-11-21 08:31:44 -0700

    build(tests):Adds enzyme

--- src/sections/IslandGame/components/Stats/Stats.test.js
+++ src/sections/IslandGame/components/Stats/Stats.test.js
@@ -1,6 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme'
 import Stats from './Stats';
 
+//OnUpdate, numOfIsland, numOfLand, numOfWater 
 it('renders count button', () => {
-  render(Stats);
+  render(<Stats></Stats>);
+});
+
+it('should render numOfIsland', () => {
+  const numOfIslandExample = 0;
+  const wrapper = shallow(<Stats numOfIsland={numOfIslandExample}></Stats>)
+  expect(wrapper.prop('numOfIsland')).toEqual(numOfIslandExample);
 });
